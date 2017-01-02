d64708d4c8a3f883cc29325e5575441a48238cd5
2012rcj <2012rcj@gmail.com>
2017-01-02 17:03:46 +0530

    Update viewCart.php

--- viewCart.php
+++ viewCart.php
@@ -169,8 +169,85 @@ table#t01 th {
 </style>
   <div id="light" class="white_content">
 <?php
-include 'Checkout.php';
+// include database configuration file
+include 'db_const.php';
+// initializ shopping cart class
+include 'Cart.php';
+$cart = new Cart;
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Checkout - PHP Shopping Cart Tutorial</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+    .container{width: 100%;padding: 50px;}
+    .table{width: 65%;float: left;}
+    .shipAddr{width: 30%;float: left;margin-left: 30px;}
+    .footBtn{width: 95%;float: left;}
+    .orderBtn {float: right;}
+    </style>
+</head>
+<body>
+<div class="container">
+    <h1>Order Preview</h1>
+    <table border="0" class="table_fields"  id="tablepaging"cellpadding="2px" cellspacing="20px" width="600px">
+    <thead>
+        <tr class="table_fields_top">
+            <th>Product</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Subtotal</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        session_start();
+        
+        if($cart->total_items() > 0){
+            //get cart items from session
+            $cartItems = $cart->contents();
+            foreach($cartItems as $item){
+            	
+$_SESSION['Prod_Nm']=$item["PROD_NM"];
+$_Session['Price']=$item["price"];
+$_Session['Qty']=$item["qty"];
+$_Session['Subtotal']=$item["subtotal"];
+$_Session['Prod_Id']=$item["PROD_ID"]
+?>
+        
+       <tr class="table_fields_top">
+            <td><?php echo $item["PROD_NM"]; ?></td>
+            <td><?php echo 'Rs'.$item["price"].''; ?></td>
+            <td><?php echo $item["qty"]; ?></td>
+            <td><?php echo 'Rs'.$item["subtotal"].''; ?></td>
+        </tr>
+        <?php } }else{ ?>
+        <tr><td colspan="4"><p>No items in your cart......</p></td>
+        <?php } ?>
+    </tbody>
+    <tfoot>
+        <tr>
+            <td colspan="3"></td>
+            <?php if($cart->total_items() > 0){
+            	$_SESSION['Total']=$cart->total();
+            	?>
+            <td class="text-center"><strong>Total <?php echo 'Rs'.$cart->total().''; ?></strong></td>
+            <?php } ?>
+    
+        <a href="index.php">  Continue Shopping</a>
+       
+    
+    
+    <td> <a href="bill.php">Place Order</a></td>
+</tr>
+
+    </tfoot>
+    </table>
 . <a href="javascript:void(0)" onclick="document.getElementById('light').style.display='none';document.getElementById('fade').style.display='none'">Close</a>
   </div>
   <div id="fade" class="black_overlay"></div>
