83331250e3cb8a08895142cfe050a8f4fafd0cc2
2012rcj <2012rcj@gmail.com>
2017-01-03 12:57:18 +0530

    Update bill.php

--- bill.php
+++ bill.php
@@ -65,22 +65,12 @@
 	
 
 	<!-- Section: intro -->
-    <section id="intro" class="intro">
-		<div class="intro-content">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-6">
-					<div class="wow fadeInDown" data-wow-offset="0" data-wow-delay="0.1s">
-					<h2 class="h-ultra">Hemas medical group</h2>
-					</div>
-					
-	
-	
-			<div class="row">
-				<div class="col-sm-3 col-md-3">
-                               
-<div class="wow fadeInRight" data-wow-delay="0.1s">
-<ul class="lead-list">
+    <div class="col-lg-6">
+						<div class="form-wrapper">
+						<div class="wow fadeInRight" data-wow-duration="2s" data-wow-delay="0.2s">
+						          
+							
+									<div class="panel-body">
 					<div class="wow fadeInUp" data-wow-delay="0.2s">
 						
 							<?php
