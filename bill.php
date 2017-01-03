c28af321469c704f0ec0b4a517347b1cdf3246fc
2012rcj <2012rcj@gmail.com>
2017-01-03 17:42:46 +0530

    Update bill.php

--- bill.php
+++ bill.php
@@ -1,4 +1,89 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Medicio landing page template for Health niche</title>
+	
+    <!-- css -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" type="text/css" href="plugins/cubeportfolio/css/cubeportfolio.min.css">
+	<link href="css/nivo-lightbox.css" rel="stylesheet" />
+	<link href="css/nivo-lightbox-theme/default/default.css" rel="stylesheet" type="text/css" />
+	<link href="css/owl.carousel.css" rel="stylesheet" media="screen" />
+    <link href="css/owl.theme.css" rel="stylesheet" media="screen" />
+	<link href="css/animate.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet">
+
+	<!-- boxed bg -->
+	<link id="bodybg" href="bodybg/bg1.css" rel="stylesheet" type="text/css" />
+	<!-- template skin -->
+	<link id="t-colors" href="color/default.css" rel="stylesheet">
+
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+
+<div id="wrapper">
+	
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+		<div class="top-area">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6 col-md-6">
+					<p class="bold text-left">Monday - Saturday, 8am to 10pm </p>
+					</div>
+					<div class="col-sm-6 col-md-6">
+					<p class="bold text-right">Call us now +62 008 65 001</p>
+					</div>
+				</div>
+			</div>
+		</div>
+        <div class="container navigation">
+		
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="index.html">
+                    <img src="img/logo.png" alt="" width="150" height="40" />
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+   
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+	
+
+	<!-- Section: intro -->
+    <section id="intro" class="intro">
+		<div class="intro-content">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6">
+					<div class="wow fadeInDown" data-wow-offset="0" data-wow-delay="0.1s">
+					<h2 class="h-ultra">Hemas medical group</h2>
+					</div>
+					
+	
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3 col-md-3">
+                                 <div class="well well-trans" style="width:500px;height:500px">
+<div class="wow fadeInRight" data-wow-delay="0.1s">
+<ul class="lead-list">
+					<div class="wow fadeInUp" data-wow-delay="0.2s">
+		
+							<?php
 // include database configuration file
 include 'db_const.php';
 
@@ -16,23 +101,8 @@ $_SESSION['sessCustomerID'] = 1;
 
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Checkout - PHP Shopping Cart Tutorial</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <style>
-    .container{width: 100%;padding: 50px;}
-    .table{width: 65%;float: left;}
-    .shipAddr{width: 30%;float: left;margin-left: 30px;}
-    .footBtn{width: 95%;float: left;}
-    .orderBtn {float: right;}
-    </style>
-</head>
-<body>
+
+
 <div class="container">
     <h1>Order Preview</h1>
     <table border="0" class="table_fields"  id="tablepaging"cellpadding="2px" cellspacing="20px" width="600px">
@@ -83,10 +153,162 @@ $_Session['Prod_Id']=$item["PROD_ID"]
        
     
     
-    <td> <a href="bill.php">Place Order</a></td>
+    
 </tr>
 
     </tfoot>
     </table>
+						</div>
+					</div></div></div></div>
+			
+					
+
+						</div>
+						
+
+
+					
+					<div class="col-lg-6">
+						<div class="form-wrapper">
+						<div class="wow fadeInRight" data-wow-duration="2s" data-wow-delay="0.2s">
+						          
+							<div class="panel panel-skin">
+							<div class="panel-heading">
+									<h3 class="panel-title"><span class="fa fa-pencil-square-o"></span> Oder Details <small>(It's free!)</small></h3>
+									</div>
+									<div class="panel-body">
+				<title>Billing Info</title>
+
+
+
+<script language="javascript">
+function FillBilling(f) {
+  if(f.billingtoo.checked == true) {
+   
+    f.Address.value = f.address.value;
+  }
+}
+ function isNumber(evt) {
+        var iKeyCode = (evt.which) ? evt.which : evt.keyCode
+        if (iKeyCode != 46 && iKeyCode > 31 && (iKeyCode < 48 || iKeyCode > 57))
+            return false;
+
+        return true;
+
+}	
+function validate(){
+		var f=document.form1;
+                if(f.mobile_no.value=='')
+                 {
+
+                alert('Your phone number is required');
+                f.mobile_no.focus();
+return false;
+
+
+}
+		if(f.name.value==''){
+			alert('Your name is required');
+			f.name.focus();
+			return false;
+		}
+
+		f.command.value='update';
+		f.submit();
+	}
+</script>  
+
+
+</head>
+
+
+
+
+
+<body> 
+
+
+
+<form name="form1" onsubmit="return validate()" method="POST" action="paybycash.php">
+    	
+<div align="center">
+      
+
+ 
+ <table  border: thin solid black;width: auto; height: auto; background-colour:lightblue >   
+
+            <tr><td>Order Total:</td><td><input type="text" readonly="readonly" value="<?php session_start();
+echo $_SESSION['Total'];?>" </td></tr>
+             
+<tr><td>Phone:</td><td><input type="text" name="mobile_no" required maxlength="10" onkeypress="javascript:return isNumber(event)" </td></tr>
+            <tr><td>Your Name:</td><td><input type="text" name="name"  /></td></tr>
+            <tr><td>Email:</td><td><input type="text" name="e_mail" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"  </td></tr>
+            <tr><td>Shipping Address:</td><td><textarea name="shippingaddress" cols="20" rows="5" ... ></textarea></td></tr>
+            <tr><td>CheckBox:</td><td><input type="checkbox" name="billingtoo" onclick="FillBilling(this.form)">
+              Check this box if Billing Address and Shipping are the same</td></tr>
+                 
+             <tr><td>Billing Address:</td><td><textarea name= "billingaddress" cols="20" rows="5" ... ></textarea></td></tr>
+            
+            
+            <tr><td>&nbsp;</td><td><input type="submit"class ="myButton" value="Confirm Order"  /></td></tr>
+        </table>
+	</div>
+</div>
+</form>
+	
+	<!-- progress bar -->
+							
+									</form>
+								</div>
+							</div>				
+						</div>
+						</div>
+						</div>
+					</div>					
+				</div>		
+			</div>
+		</div>		
+    </section>
+	
+	<!-- /Section: intro -->
+
+	<!-- Section: boxes -->
+    <
+	</footer>
+
+</div>
+<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+
+	<!-- Core JavaScript Files -->
+    <script src="js/jquery.min.js"></script>	 
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
+	<script src="js/wow.min.js"></script>
+	<script src="js/jquery.scrollTo.js"></script>
+	<script src="js/jquery.appear.js"></script>
+	<script src="js/stellar.js"></script>
+	<script src="plugins/cubeportfolio/js/jquery.cubeportfolio.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/nivo-lightbox.min.js"></script>
+    <script src="js/custom.js"></script>
+<script type="text/javascript" src="progressbar/jquery.min.js"></script>
+	<script type="text/javascript" src="progressbar/jquery.form.min.js"></script>
+	
+	<!-- main script -->
+	<script type="text/javascript" src="progressbar/script.js"></script>
+<script type="text/javascript">
+<!--
+    function toggle_visibility(id) {
+      
+        var e = document.getElementById(id);
+    
+        e.style.display = "block";
+     
+}
+//-->
+</script>
+</body>
+
+</html>
 </body>
 </html>
