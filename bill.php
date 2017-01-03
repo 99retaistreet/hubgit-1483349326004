409a35a30fce9f8abcee00ef82895a25cf99ec95
2012rcj <2012rcj@gmail.com>
2017-01-03 18:12:08 +0530

    Update bill.php

--- bill.php
+++ bill.php
@@ -100,7 +100,7 @@ $_SESSION['sessCustomerID'] = 1;
 
 <div class="container">
     <h1>Order Preview</h1>
-    <table border="0" class="table_fields"  id="tablepaging"cellpadding="2px" cellspacing="20px" width="600px">
+    <table border="0" class="table_fields"  id="tablepaging"cellpadding="2px" cellspacing="20px">
     <thead>
         <tr class="table_fields_top">
             <th>Product</th>
