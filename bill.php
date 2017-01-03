d853582122bea212038b0f6edfed616d8c48f396
2012rcj <2012rcj@gmail.com>
2017-01-03 12:23:49 +0530

    Update bill.php

--- bill.php
+++ bill.php
@@ -110,7 +110,7 @@ $_SESSION['sessCustomerID'] = 1;
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <style>
-    .container{width: 100%;padding: 50px;}
+    .
     .table{width: 65%;float: left;}
     .shipAddr{width: 30%;float: left;margin-left: 30px;}
     .footBtn{width: 95%;float: left;}
