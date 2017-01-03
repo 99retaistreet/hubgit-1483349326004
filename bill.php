4469623665a4725e1ce03604a037c3b72dc86f05
2012rcj <2012rcj@gmail.com>
2017-01-03 19:08:16 +0530

    Update bill.php

--- bill.php
+++ bill.php
@@ -137,7 +137,7 @@ $_Session['Prod_Id']=$item["PROD_ID"]
     </tbody>
     <tfoot>
         <tr>
-            <td colspan="2"></td>
+            <td colspan="0"></td>
             <?php if($cart->total_items() > 0){
             	$_SESSION['Total']=$cart->total();
             	?>
