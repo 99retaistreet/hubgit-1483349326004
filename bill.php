9b3d9430bddcd49ffc1692c4df8102215e70348d
2012rcj <2012rcj@gmail.com>
2017-01-03 16:52:30 +0530

    Update bill.php

--- bill.php
+++ bill.php
@@ -1,85 +1,245 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
-<title>Billing Info</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
+    <title>Medicio landing page template for Health niche</title>
+	
+    <!-- css -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" type="text/css" href="plugins/cubeportfolio/css/cubeportfolio.min.css">
+	<link href="css/nivo-lightbox.css" rel="stylesheet" />
+	<link href="css/nivo-lightbox-theme/default/default.css" rel="stylesheet" type="text/css" />
+	<link href="css/owl.carousel.css" rel="stylesheet" media="screen" />
+    <link href="css/owl.theme.css" rel="stylesheet" media="screen" />
+	<link href="css/animate.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet">
 
-
-<script language="javascript">
-function FillBilling(f) {
-  if(f.billingtoo.checked == true) {
-   
-    f.Address.value = f.address.value;
-  }
-}
- function isNumber(evt) {
-        var iKeyCode = (evt.which) ? evt.which : evt.keyCode
-        if (iKeyCode != 46 && iKeyCode > 31 && (iKeyCode < 48 || iKeyCode > 57))
-            return false;
-
-        return true;
-
-}	
-function validate(){
-		var f=document.form1;
-                if(f.mobile_no.value=='')
-                 {
-
-                alert('Your phone number is required');
-                f.mobile_no.focus();
-return false;
-
-
-}
-		if(f.name.value==''){
-			alert('Your name is required');
-			f.name.focus();
-			return false;
-		}
-
-		f.command.value='update';
-		f.submit();
-	}
-</script>  
+	<!-- boxed bg -->
+	<link id="bodybg" href="bodybg/bg1.css" rel="stylesheet" type="text/css" />
+	<!-- template skin -->
+	<link id="t-colors" href="color/default.css" rel="stylesheet">
 
 
 </head>
 
+<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+
+<div id="wrapper">
+	
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+		<div class="top-area">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6 col-md-6">
+					<p class="bold text-left">Monday - Saturday, 8am to 10pm </p>
+					</div>
+					<div class="col-sm-6 col-md-6">
+					<p class="bold text-right">Call us now +62 008 65 001</p>
+					</div>
+				</div>
+			</div>
+		</div>
+        <div class="container navigation">
+		
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="index.html">
+                    <img src="img/logo.png" alt="" width="150" height="40" />
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+			  <ul class="nav navbar-nav">
+				<li class="active"><a href="#intro">Home</a></li>
+				<li><a href="#service">Service</a></li>
+				<li><a href="#doctor">Doctors</a></li>
+				<li><a href="#facilities">Facilities</a></li>
+				<li><a href="#pricing">Pricing</a></li>
+				<li class="dropdown">
+				  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="badge custom-badge red pull-right">Extra</span>More <b class="caret"></b></a>
+				  <ul class="dropdown-menu">
+					<li><a href="index.html">Home form</a></li>
+					<li><a href="index-video.html">Home video</a></li>
+					<li><a href="index-cta.html">Home CTA</a></li>
+					<li><a href="https://bootstrapmade.com">Download</a></li>
+				  </ul>
+				</li>
+			  </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+	
+
+	<!-- Section: intro -->
+    <section id="intro" class="intro">
+		<div class="intro-content">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6">
+					<div class="wow fadeInDown" data-wow-offset="0" data-wow-delay="0.1s">
+					<h2 class="h-ultra">Hemas medical group</h2>
+					</div>
+					
+	
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3 col-md-3">
+                                 <div class="well well-trans">
+<div class="wow fadeInRight" data-wow-delay="0.1s">
+<ul class="lead-list">
+					<div class="wow fadeInUp" data-wow-delay="0.2s">
+						<div class="box text-center">
+							
+							
+							<h4 class="h-bold"><a href="#" onclick="toggle_visibility('foo');">Upload Your Priscription</h4>
+							<h1>Order Preview</h1>
+    <table border="0" class="table_fields"  id="tablepaging"cellpadding="2px" cellspacing="20px" width="600px">
+    <thead>
+        <tr class="table_fields_top">
+            <th>Product</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Subtotal</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        session_start();
+        
+        if($cart->total_items() > 0){
+            //get cart items from session
+            $cartItems = $cart->contents();
+            foreach($cartItems as $item){
+            	
+$_SESSION['Prod_Nm']=$item["PROD_NM"];
+$_Session['Price']=$item["price"];
+$_Session['Qty']=$item["qty"];
+$_Session['Subtotal']=$item["subtotal"];
+$_Session['Prod_Id']=$item["PROD_ID"]
+?>
+        
+       <tr class="table_fields_top">
+            <td><?php echo $item["PROD_NM"]; ?></td>
+            <td><?php echo 'Rs'.$item["price"].''; ?></td>
+            <td><?php echo $item["qty"]; ?></td>
+            <td><?php echo 'Rs'.$item["subtotal"].''; ?></td>
+        </tr>
+        <?php } }else{ ?>
+        <tr><td colspan="4"><p>No items in your cart......</p></td>
+        <?php } ?>
+    </tbody>
+    <tfoot>
+        <tr>
+            <td colspan="3"></td>
+            <?php if($cart->total_items() > 0){
+            	$_SESSION['Total']=$cart->total();
+            	?>
+            <td class="text-center"><strong>Total <?php echo 'Rs'.$cart->total().''; ?></strong></td>
+            <?php } ?>
+    
+        <a href="index.php">  Continue Shopping</a>
+       
+    
+    
+    <td> <a href="bill.php">Place Order</a></td>
+</tr>
+
+    </tfoot>
+    </table>
+
+                                               </ul>	</ul>
+						</div>
+					</div></div></div></div>
+			
+					
+
+						</div>
+						
 
 
-
-
-<body> 
-
-
-
-<form name="form1" onsubmit="return validate()" method="POST" action="paybycash.php">
-    	
-<div align="center">
-        <h1 align="center">Billing Info</h1> 
-
- 
- <table  width="500" height="500" border="0"  cellpadding="1px"  style="background-color: #6495ED">   
-
-            <tr><td>Order Total:</td><td><input type="text" readonly="readonly" value="<?php session_start();
-echo $_SESSION['Total'];?>" </td></tr>
-             
-<tr><td>Phone:</td><td><input type="text" name="mobile_no" required maxlength="10" onkeypress="javascript:return isNumber(event)" </td></tr>
-            <tr><td>Your Name:</td><td><input type="text" name="name"  /></td></tr>
-            <tr><td>Email:</td><td><input type="text" name="e_mail" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"  </td></tr>
-            <tr><td>Shipping Address:</td><td><textarea name="shippingaddress" cols="20" rows="5" ... ></textarea></td></tr>
-            <tr><td>CheckBox:</td><td><input type="checkbox" name="billingtoo" onclick="FillBilling(this.form)">
-              Check this box if Billing Address and Shipping are the same</td></tr>
-                 
-             <tr><td>Billing Address:</td><td><textarea name= "billingaddress" cols="20" rows="5" ... ></textarea></td></tr>
-            
-            
-            <tr><td>&nbsp;</td><td><input type="submit"class ="myButton" value="Confirm Order"  /></td></tr>
-        </table>
+					<div id="foo" style="display:none;">
+					<div class="col-lg-6">
+						<div class="form-wrapper">
+						<div class="wow fadeInRight" data-wow-duration="2s" data-wow-delay="0.2s">
+						          
+							<div class="panel panel-skin">
+							<div class="panel-heading">
+									<h3 class="panel-title"><span class="fa fa-pencil-square-o"></span> Upload Your Priscription <small>(It's free!)</small></h3>
+									</div>
+									<div class="panel-body">
+				<form action="upload.php" method="post" enctype="multipart/form-data" class="pure-form">
+		<input type="file" name="files[]" multiple="multiple" id="files">
+		<input type="submit" value="Upload" class="pure-button pure-button-primary">
+	</form>
+	
+	<!-- progress bar -->
+	<div class="progress">
+		<div class="bar"></div >
+		<div class="percent">0%</div >
 	</div>
+
+</div> later</p>
+									
+									</form>
+								</div>
+							</div>				
+						</div>
+						</div>
+						</div>
+					</div>					
+				</div>		
+			</div>
+		</div>		
+    </section>
+	
+	<!-- /Section: intro -->
+
+	<!-- Section: boxes -->
+    <
+	</footer>
+
 </div>
-</form>
+<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+
+	<!-- Core JavaScript Files -->
+    <script src="js/jquery.min.js"></script>	 
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
+	<script src="js/wow.min.js"></script>
+	<script src="js/jquery.scrollTo.js"></script>
+	<script src="js/jquery.appear.js"></script>
+	<script src="js/stellar.js"></script>
+	<script src="plugins/cubeportfolio/js/jquery.cubeportfolio.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/nivo-lightbox.min.js"></script>
+    <script src="js/custom.js"></script>
+<script type="text/javascript" src="progressbar/jquery.min.js"></script>
+	<script type="text/javascript" src="progressbar/jquery.form.min.js"></script>
+	
+	<!-- main script -->
+	<script type="text/javascript" src="progressbar/script.js"></script>
+<script type="text/javascript">
+<!--
+    function toggle_visibility(id) {
+      
+        var e = document.getElementById(id);
+    
+        e.style.display = "block";
+     
+}
+//-->
+</script>
 </body>
 
 </html>
