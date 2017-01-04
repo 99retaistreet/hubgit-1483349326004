b832ed0f0193778225a06493f6dee4d0374ab39a
2012rcj <2012rcj@gmail.com>
2017-01-04 17:48:15 +0530

    Update billing.php

--- billing.php
+++ billing.php
@@ -111,21 +111,38 @@
 									<div class="panel-body">
 
 <?php
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/png"))
+
+&& ($_FILES["file"]["size"] < 100000000))
+{
+if ($_FILES["file"]["error"] > 0)
+{
+echo "File Error : " . $_FILES["file"]["error"] . "<br />";
+}else {
+ $_FILES["file"]["name"] . "<br />";
+ $_FILES["file"]["type"] . "<br />";
+ ($_FILES["file"]["size"] / 1024) . " Kb<br />"; 
 
 
+if (file_exists("images/".$_FILES["file"]["name"]))
+{
+echo "<b>".$_FILES["file"]["name"] . " already exists. </b>";
+}else
+{
+move_uploaded_file($_FILES["file"]["tmp_name"],"images/". $_FILES["file"]["name"]);
+"Stored in: " . "images/" . $_FILES["file"]["name"]."<br />";
+?>
 
-$fileContent = file_get_contents($_FILES["file"]["tmp_name"]);
-$imageData = base64_encode(file_get_contents($_FILES["file"]["tmp_name"]));
-
-// Format the image SRC:  data:{mime};base64,{data};
-$file_name=$_FILES['file']['name'];
-	echo "<img src= .$file_name.  height=200 width=300 />";
-
-   
-$_SESSION['fil'] = "$src";
-
-  
-
+<img src="images/<?php echo $_FILES["file"]["name"]; ?>" alt="Image path Invalid" >
+<?php
+}
+}
+}else
+{
+echo "Invalid file detail ::<br> file type ::".$_FILES["file"]["type"]." , file size::: ".$_FILES["file"]["size"];
+}
 ?>
 
 										</div>
