bd2b264d57b9c147816d15b63c6d3edfefe96b91
2012rcj <2012rcj@gmail.com>
2017-01-04 16:52:16 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,9 +1,21 @@
+<?php
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/pjpeg"))
+&& ($_FILES["file"]["size"] < 10000000000))
+{
 
-<html>
 
 
 
+?>
 
 <img src="images/<?php echo $_FILES["file"]["name"]; ?>" alt="Image path Invalid" >
+<?php
 
-</html>
+
+}else
+{
+echo "Invalid file detail ::<br> file type ::".$_FILES["file"]["type"]." , file size::: ".$_FILES["file"]["size"];
+}
+?>
