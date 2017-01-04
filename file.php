14bb84d30370c91c395c688e2de1a372955d6c4d
2012rcj <2012rcj@gmail.com>
2017-01-04 17:49:24 +0530

    Update file.php

--- file.php
+++ file.php
@@ -110,7 +110,7 @@
 									<h3 class="panel-title"><span class="fa fa-pencil-square-o"></span> Upload Your Priscription </h3>
 									</div>
 									<div class="panel-body">
-			<form action="upload.php" method="POST" enctype="multipart/form-data">
+			<form action="billing.php" method="POST" enctype="multipart/form-data">
 	            	<input type="file" name="file" /><br>
 	            	<input type="submit" class="btn" value="Upload">
 	          	</form>
