13dfb61abbd943142660b40d8a5b64a8567de161
2012rcj <2012rcj@gmail.com>
2017-01-04 17:44:39 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -9,10 +9,10 @@ if ($_FILES["file"]["error"] > 0)
 {
 echo "File Error : " . $_FILES["file"]["error"] . "<br />";
 }else {
-echo "Upload File Name: " . $_FILES["file"]["name"] . "<br />";
-echo "File Type: " . $_FILES["file"]["type"] . "<br />";
-echo "File Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />"; 
-echo "File Description:: ".$_POST['description']."<br />";
+ $_FILES["file"]["name"] . "<br />";
+ $_FILES["file"]["type"] . "<br />";
+ ($_FILES["file"]["size"] / 1024) . " Kb<br />"; 
+
 
 if (file_exists("images/".$_FILES["file"]["name"]))
 {
@@ -20,9 +20,9 @@ echo "<b>".$_FILES["file"]["name"] . " already exists. </b>";
 }else
 {
 move_uploaded_file($_FILES["file"]["tmp_name"],"images/". $_FILES["file"]["name"]);
-echo "Stored in: " . "images/" . $_FILES["file"]["name"]."<br />";
+"Stored in: " . "images/" . $_FILES["file"]["name"]."<br />";
 ?>
-Uploaded File:<br>
+
 <img src="images/<?php echo $_FILES["file"]["name"]; ?>" alt="Image path Invalid" >
 <?php
 }
