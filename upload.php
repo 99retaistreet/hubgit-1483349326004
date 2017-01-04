b19d68e3d4215ba852b41ff6719367d9d59a0e72
2012rcj <2012rcj@gmail.com>
2017-01-04 16:36:10 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,81 +1,33 @@
 <?php
-session_start();
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/pjpeg"))
+&& ($_FILES["file"]["size"] < 100000))
+{
+if ($_FILES["file"]["error"] > 0)
+{
+echo "File Error : " . $_FILES["file"]["error"] . "<br />";
+}else {
+echo "Upload File Name: " . $_FILES["file"]["name"] . "<br />";
+echo "File Type: " . $_FILES["file"]["type"] . "<br />";
+echo "File Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />"; 
+echo "File Description:: ".$_POST['description']."<br />";
 
-//require 'db_const.php';
-//code modified from https://github.com/ibmjstart/wp-bluemix-objectstorage/blob/master/classes/swift.php
-$vcap = getenv("VCAP_SERVICES");
-$data = json_decode($vcap, true);
-$creds = $data['Object-Storage']['0']['credentials'];
-$auth_url = "https://identity.open.softlayer.com" . '/v3'; //keystone v3
-$region = "dallas";
-$userId = "a95f736f4c224fbf86962f3d10766762";
-$password = "Bg3m.I8AJ^H~el9[";
-$projectId = "e283fcb86dbb4286a524064bb689f889";
-$openstack = new OpenStack\OpenStack([
-			    'authUrl' => $auth_url,
-			    'region'  => $region,
-			    'user'    => [
-			        'id'       => $userId,
-			        'password' => $password
-			    ],
-			    'scope'   => [
-			    	'project' => [
-			    		'id' => $projectId
-			    	]
-			    ]
-			]);
-//creates the container if it does not already exist
-$openstack->objectStoreV1()->createContainer(['name' => 'Rohit']);
-$container = $openstack->objectStoreV1()
-                       ->getContainer('Rohit');
-                       
-//found on http://stackoverflow.com/questions/16888722/get-content-of-file-uploaded-by-user-before-saving
-$fileContent = file_get_contents($_FILES["file"]["tmp_name"]);
-$options = [
-    'name'    => $_FILES['file']['name'],
-    'content' => $fileContent
-];
-
-
-
-$name =$_FILES["file"]["name"];
-//$content=$options['content'];
-//echo "$content";
-//echo '<img src= "'.$containe .'" height=200 width=150>';
-
-//echo "<hr />Image File Uploaded : <br /><img src="$options['name']" style="width:100%  " />";
-$container->createObject($options);
-
-//found on http://stackoverflow.com/questions/14810399/php-form-redirect
-//header( 'Location:billing.php' ) ;
-
-session_start();
-
-
- $Store_Id='1465804506';
-$pres_id=time();
-//echo $password;
-
-require("db_const.php");
-
-
-
-
-
-$stmt = db2_prepare($conn,"INSERT INTO RETAIL_PRISCRIPTION(PRISCRIPTION,IMAGE,STORE_ID) VALUES('$pres_id','$name','$Store_Id')");
-
-
-
-
-if (!db2_execute($stmt)) {
-    printf("%s\n", db2_stmt_error($stmt));
-    $err = db2_stmt_errormsg();
-    die($err . "\n");
-}
-//echo "<script type='text/javascript'>window.location.href = 'index.php';</script>";
-           
-
-
-
+if (file_exists("images/".$_FILES["file"]["name"]))
+{
+echo "<b>".$_FILES["file"]["name"] . " already exists. </b>";
+}else
+{
+move_uploaded_file($_FILES["file"]["tmp_name"],"images/". $_FILES["file"]["name"]);
+echo "Stored in: " . "images/" . $_FILES["file"]["name"]."<br />";
 ?>
-
+Uploaded File:<br>
+<img src="images/<?php echo $_FILES["file"]["name"]; ?>" alt="Image path Invalid" >
+<?php
+}
+}
+}else
+{
+echo "Invalid file detail ::<br> file type ::".$_FILES["file"]["type"]." , file size::: ".$_FILES["file"]["size"];
+}
+?>
