16eb1214ffa90c8bd7f1f316aa2083fab7e0480b
2012rcj <2012rcj@gmail.com>
2017-01-05 19:28:37 +0530

    Update search.php

--- search.php
+++ search.php
@@ -1,26 +1,48 @@
 <?php 
-//load database connection
- include 'db_const.php';
-// Search from MySQL database table
-$search=$_POST['search'];
-$stmt = "select * from RETAIL_STORE_PROD where PROD_NM LIKE '%$search%' and STORE_ID='1472222821'  LIMIT 0 , 10";
- $result = db2_prepare($conn, $stmt);
+$button = $_GET [ 'submit' ]; 
+$search = $_GET [ 'search' ]; 
+if( !$button ) 
+echo "you didn't submit a keyword";
+ else 
+{ 
+if( strlen( $search ) <= 1 ) 
+echo "Search term too short"; 
+else 
+{ 
+echo "You searched for <b> $search </b> <hr size='1' > </ br > ";
+include 'db_const.php';
+$search_exploded = explode ( " ", $search ); 
+$x = 0; 
+foreach( $search_exploded as $search_each )
+ { 
+$x++; 
+$construct = ""; 
+if( $x == 1 ) 
+$construct .="keywords LIKE '%$search_each%'"; 
+else 
+$construct .="AND keywords LIKE '%$search_each%'"; 
+} 
+$construct = " SELECT * FROM RETAIL_STORE_PROD WHERE $construct and STORE_ID='1472222821' "; 
+$result = db2_prepare($conn, $construct);
        db2_execute($result);
 // Display search result
-         if (!db2_num_rows == 0) {
-		 		echo "Search found :<br/>";
-				echo "<table style=\"font-family:arial;color:#333333;\">";	
-                echo "<tr><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;background:#98bf21;\">PRODUCTS_NAME</td><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;background:#98bf21;\">Price</td></tr>";				
-            while ($results = db2_fetch_assoc($result)) {
-				echo "<tr><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;\">";			
-                echo $results['PROD_NM'];
-				echo "</td><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;\">";
-                echo $results['MRP'];
-				
-				echo "</td></tr>";				
-            }
-				echo "</table>";		
-        } else {
-            echo 'Nothing found';
-        }
+         if (!db2_num_rows == 0)
+
+
+echo "Sorry, there are no matching result for <b> $search </b>. </br> </br> 
+1. Try more general words. for example: If you want to search 'how to create a website' then use general keyword like 'create' 'website' </br> 
+2. Try different words with similar meaning </br> 
+3. Please check your spelling"; 
+else 
+{ 
+echo "$foundnum results found !<p>"; 
+while( $runrows = mysql_fetch_assoc( $run ) ) 
+{ 
+$title = $runrows ['PROD_NM']; 
+$desc = $runrows ['MRP'];  
+echo " <b> $title </b> </a> <br> $desc <br> <p>"; 
+} 
+} 
+} 
+} 
 ?>
