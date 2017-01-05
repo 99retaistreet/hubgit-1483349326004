8cc742db8710c680280043967031c8bb9253b7ef
2012rcj <2012rcj@gmail.com>
2017-01-05 17:35:22 +0530

    Update search.php

--- search.php
+++ search.php
@@ -10,7 +10,7 @@ $stmt = "select * from RETAIL_STORE_PROD where PROD_NM LIKE '%$search%' and STOR
          if (!db2_num_rows == 0) {
 		 		echo "Search found :<br/>";
 				echo "<table style=\"font-family:arial;color:#333333;\">";	
-                echo "<tr><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;background:#98bf21;\">Title Books</td><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;background:#98bf21;\">Author</td><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;background:#98bf21;\">Price</td></tr>";				
+                echo "<tr><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;background:#98bf21;\">PRODUCTS_NAME</td><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;background:#98bf21;\">Price</td></tr>";				
             while ($results = db2_fetch_assoc($result)) {
 				echo "<tr><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;\">";			
                 echo $results['PROD_NM'];
