e2d802e86e31e74b5df3bd2cc3417ec3522eb8ac
2012rcj <2012rcj@gmail.com>
2017-01-10 16:53:26 +0530

    Update checkdata.php

--- checkdata.php
+++ checkdata.php
@@ -1,46 +1,11 @@
 <?php
-require("db_const.php");
+include 'db_const.php';
+$sql = "SELECT * FROM RETAIL_CUST ";
+$stmt = db2_prepare($conn, $sql);
+$result = db2_execute($stmt);
 
-if(isset($_POST['mobile_no'];))
-{
- $mobile_no=$_POST['mobile_no'];
-
- $checkdata=" SELECT MOBILE_NO FROM RETAIL_CUST WHERE MOBILE_NO='$mobile_no' ";
-
-$result = db2_prepare($conn, $checkdata);
-       db2_execute($result);
-
-
- if(db2_num_rows($result)>0)
- {
-  echo "Mobile Number Already Exist";
- }
- else
- {
-  echo "OK";
- }
- exit();
+while ($row = db2_fetch_assoc($stmt)) {
+     
+    echo    $row['NAME'];
 }
-
-if(isset($_POST['e_mail']))
-{
- $e_mail=$_POST['e_mail'];
-
- $checkdata=" SELECT E_MAIL FROM RETAIL_CUST WHERE E_MAIL='$e_mail' ";
-  $result = db2_prepare($conn, $checkdata);
-       db2_execute($result);
-
-
- if(db2_num_rows($result)>0)
- {
-  echo "E_Mail Already Exist";
- }
- else
- {
-  echo "OK";
- }
- exit();
-}
-
-
 ?>
