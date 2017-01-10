cd1f4f2c709df5b5f6f445ea761e197922684085
2012rcj <2012rcj@gmail.com>
2017-01-10 20:13:57 +0530

    Update bill.php

--- bill.php
+++ bill.php
@@ -1,67 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Medicio landing page template for Health niche</title>
-	
-    <!-- css -->
-    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-	<link rel="stylesheet" type="text/css" href="plugins/cubeportfolio/css/cubeportfolio.min.css">
-	<link href="css/nivo-lightbox.css" rel="stylesheet" />
-	<link href="css/nivo-lightbox-theme/default/default.css" rel="stylesheet" type="text/css" />
-	<link href="css/owl.carousel.css" rel="stylesheet" media="screen" />
-    <link href="css/owl.theme.css" rel="stylesheet" media="screen" />
-	<link href="css/animate.css" rel="stylesheet" />
-    <link href="css/style.css" rel="stylesheet">
-
-	<!-- boxed bg -->
-	<link id="bodybg" href="bodybg/bg1.css" rel="stylesheet" type="text/css" />
-	<!-- template skin -->
-	<link id="t-colors" href="color/default.css" rel="stylesheet">
-
-
-</head>
-
-<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
-
-<div id="wrapper">
-	
-    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
-		<div class="top-area">
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-6 col-md-6">
-					<p class="bold text-left">Monday - Saturday, 8am to 10pm </p>
-					</div>
-					<div class="col-sm-6 col-md-6">
-					<p class="bold text-right">Call us now +62 008 65 001</p>
-					</div>
-				</div>
-			</div>
-		</div>
-        <div class="container navigation">
-		
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
-                    <i class="fa fa-bars"></i>
-                </button>
-                <a class="navbar-brand" href="index.html">
-                    <img src="img/logo.png" alt="" width="150" height="40" />
-                </a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-   
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+<?php
+// include database configuration file
+include 'header.php';
+?>
 	
 
 	<!-- Section: intro -->
