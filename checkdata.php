a7805055b8180e8cbd5a735eddaba575ef9b3a7b
2012rcj <2012rcj@gmail.com>
2017-01-10 17:30:30 +0530

    Update checkdata.php

--- checkdata.php
+++ checkdata.php
@@ -6,6 +6,9 @@ SESSION_START();
 
 	$CUST_ID=$_SESSION['CUST_id'];
 
+echo $Mobile_No;
+echo $CUST_ID;
+
 $sql = "SELECT * FROM RETAIL_CUST  where MOBILE_NO='$Mobile_No' and CUST_ID='$CUST_ID' LIMIT 1 ";
 $stmt = db2_prepare($conn, $sql);
 $result = db2_execute($stmt);
