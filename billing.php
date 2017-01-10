aa9b2fda27141456b17c42492f1e82fd1bb252e7
2012rcj <2012rcj@gmail.com>
2017-01-10 19:10:19 +0530

    Update billing.php

--- billing.php
+++ billing.php
@@ -5,9 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content="">   
 
-    <title>99 retail Solution Pvt Ltd</title>
+    <title>99 Retail Pvt Ltd</title>
 	
     <!-- css -->
     <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
@@ -17,7 +17,6 @@
 	<link href="css/nivo-lightbox-theme/default/default.css" rel="stylesheet" type="text/css" />
 	<link href="css/owl.carousel.css" rel="stylesheet" media="screen" />
     <link href="css/owl.theme.css" rel="stylesheet" media="screen" />
-      <link href="simpletreemenu.js" rel="stylesheet"  />
 	<link href="css/animate.css" rel="stylesheet" />
     <link href="css/style.css" rel="stylesheet">
 
@@ -31,7 +30,7 @@
 
 <body id="page-top" data-spy="scroll" data-target=".navbar-custom">
 
-<div id="wrapper">  
+<div id="wrapper">
 	
     <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
 		<div class="top-area">
@@ -41,7 +40,7 @@
 					<p class="bold text-left">Monday - Saturday, 8am to 10pm </p>
 					</div>
 					<div class="col-sm-6 col-md-6">
-					<p class="bold text-right">Call us now ---</p>
+					<p class="bold text-right">Call us now </p>
 					</div>
 				</div>
 			</div>
@@ -57,11 +56,47 @@
                 </a>
             </div>
 
-           
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+           <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+
+
+	<ul class="nav navbar-nav">
+		<li class="active">
+			<?php
+include 'db_const.php';
+SESSION_START();
+
+  $Mobile_No=$_SESSION['Mobile_No'];
+
+	$CUST_ID=$_SESSION['CUST_id'];
+
+
+
+$sql = "SELECT * FROM RETAIL_CUST  where CUST_ID='$CUST_ID' LIMIT 1 ";
+$stmt = db2_prepare($conn, $sql);
+$result = db2_execute($stmt);
+while ($row = db2_fetch_assoc($stmt)) {
+     
+     $name="<b>".$row['NAME'] ."</b>";
+    echo"<b>";
+    echo "Hello $name";
+    echo "</b>";
+}
+?></li>
+<li class="dropdown">
+<a href="#" class="dropdown-toggle" data-toggle="dropdown">More <b class="caret">
+</b>
+</a>
+<ul class="dropdown-menu">
+<li><a href="logout.php">Logout</a></li>
+
+</ul>
+</li>
+</ul>
+</div>
+<!-- /.navbar-collapse -->
+ </div>
+ <!-- /.container -->
+  </nav>
 	
 
 	<!-- Section: intro -->
