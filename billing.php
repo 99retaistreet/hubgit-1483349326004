24b91b581dc166ba1e4993b04139c86288d12e4e
2012rcj <2012rcj@gmail.com>
2017-01-11 12:40:24 +0530

    Update billing.php

--- billing.php
+++ billing.php
@@ -1,6 +1,102 @@
-<?php
-include 'header.php';
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">   
+
+    <title>99 Retail Pvt Ltd</title>
+	
+    <!-- css -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" type="text/css" href="plugins/cubeportfolio/css/cubeportfolio.min.css">
+	<link href="css/nivo-lightbox.css" rel="stylesheet" />
+	<link href="css/nivo-lightbox-theme/default/default.css" rel="stylesheet" type="text/css" />
+	<link href="css/owl.carousel.css" rel="stylesheet" media="screen" />
+    <link href="css/owl.theme.css" rel="stylesheet" media="screen" />
+	<link href="css/animate.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet">
+
+	<!-- boxed bg -->
+	<link id="bodybg" href="bodybg/bg1.css" rel="stylesheet" type="text/css" />
+	<!-- template skin -->
+	<link id="t-colors" href="color/default.css" rel="stylesheet">
+
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+
+<div id="wrapper">
+	
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+		<div class="top-area">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6 col-md-6">
+					<p class="bold text-left">Monday - Saturday, 8am to 10pm </p>
+					</div>
+					<div class="col-sm-6 col-md-6">
+					<p class="bold text-right">Call us now </p>
+					</div>
+				</div>
+			</div>
+		</div>
+        <div class="container navigation">
+		
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="index.html">
+                    <img src="img/logo.png" alt="" width="150" height="40" />
+                </a>
+            </div>
+
+           <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+
+
+	<ul class="nav navbar-nav">
+		<li class="active">
+			<?php
+include 'db_const.php';
+SESSION_START();
+
+  $Mobile_No=$_SESSION['Mobile_No'];
+
+	$CUST_ID=$_SESSION['CUST_id'];
+
+
+
+$sql = "SELECT * FROM RETAIL_CUST  where CUST_ID='$CUST_ID' LIMIT 1 ";
+$stmt = db2_prepare($conn, $sql);
+$result = db2_execute($stmt);
+while ($row = db2_fetch_assoc($stmt)) {
+     
+     $name="<b>".$row['NAME'] ."</b>";
+    echo"<b>";
+    echo "Hello $name";
+    echo "</b>";
+}
+?></li>
+<li class="dropdown">
+<a href="#" class="dropdown-toggle" data-toggle="dropdown">More <b class="caret">
+</b>
+</a>
+<ul class="dropdown-menu">
+<li><a href="logout.php">Logout</a></li>
+
+</ul>
+</li>
+</ul>
+</div>
+<!-- /.navbar-collapse -->
+ </div>
+ <!-- /.container -->
+  </nav>
 	
 
 	<!-- Section: intro -->
@@ -10,7 +106,7 @@ include 'header.php';
 				<div class="row">
 					<div class="col-lg-6">
 					<div class="wow fadeInDown" data-wow-offset="0" data-wow-delay="0.1s">
-					<h2 class="h-ultra">Pierson medical group</h2>
+					<h2 class="h-ultra">Hemas medical group</h2>
 					</div>
 					
 	
@@ -28,14 +124,14 @@ include 'header.php';
 							<p>
 							The Priscription you have Uploaded should be given by the doctors consult 							</p>
 
-                                               </ul>
+                                               </ul>	</ul>
 						</div>
 					</div></div></div></div>
 			
 					
 
 						</div>
-						</div></div>
+						
 
 
 					
@@ -130,12 +226,11 @@ Add some more medicines beyond your priscription
 </div>
 </div>
 </div>
-	<div class="container" align="center">
 <div class="col-sm-3 col-md-3">
 <div class="wow fadeInUp" data-wow-delay="0.2s">
 <div class="box text-center">
 <i class="fa fa-list-alt fa-3x circled bg-skin"></i>
-<h4 class="h-bold"><a href="#section" onclick="toggle_visibility('foo');">Make Billing</a></h4>
+<h4 class="h-bold"><a href="#section2" onclick="toggle_visibility('foo');">Make Billing</a></h4>
 <p>
 Fill your address and order will be delivered at home  
 </p>
@@ -143,11 +238,10 @@ Fill your address and order will be delivered at home
 </div>
 </div>
 				
-</div>
+
 
 </div>
-	<div id="section">
-
+	<div id="section1">
 <div id="foo" class="wow fadeInDown" style="display:none;">
 <section id="boxes" class="home-section paddingtop-80">
 <div class="container">
@@ -207,7 +301,7 @@ return false;
 </script>  
 
 
-
+</head>
 
 
 
@@ -245,7 +339,7 @@ echo $_SESSION['Total'];?>" /></td></tr>
 </form>
 </body>
 	
-</form>
+
 				</div></div>
 					</div>
 				</div>
@@ -289,7 +383,7 @@ include 'db_const.php';
 	padding: 10px 20px;
 	border: 1px solid #006699;
 	border-radius: 4px;
-	background-color:#00978DF;
+	background-color: #00978DF;
 	color: White;
 	text-decoration: none;
 	font-family: Arial,Helvetica,sans-serif;
@@ -458,7 +552,7 @@ function popup(mylink, windowname)
   
         <?php
        $count=0;
-        //get rows query    
+        //get rows query
         $stmt = "SELECT * FROM RETAIL_STORE_PROD WHERE STORE_ID='1472222821'";
        $result = db2_prepare($conn, $stmt);
        db2_execute($result);
@@ -521,7 +615,7 @@ pager.showPage(1);
 
     </div>
 </div>
-
+<>    
 
 <a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
 
