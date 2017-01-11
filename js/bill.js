5977b450836c78fc02ec152ac0d512b1e48ba810
2012rcj <2012rcj@gmail.com>
2017-01-11 11:46:21 +0530

    Add files via upload

---
+++ js/bill.js
@@ -0,0 +1,168 @@
+<script language="javascript">
+function FillBilling(f) {
+  if(f.billingtoo.checked == true) {
+   
+    f.Address.value = f.address.value;
+  }
+}
+ function isNumber(evt) {
+        var iKeyCode = (evt.which) ? evt.which : evt.keyCode
+        if (iKeyCode != 46 && iKeyCode > 31 && (iKeyCode < 48 || iKeyCode > 57))
+            return false;
+
+        return true;
+
+}	
+function validate(){
+		var f=document.form1;
+                if(f.mobile_no.value=='')
+                 {
+
+                alert('Your phone number is required');
+                f.mobile_no.focus();
+return false;
+
+
+}
+		if(f.name.value==''){
+			alert('Your name is required');
+			f.name.focus();
+			return false;
+		}
+
+		f.command.value='update';
+		f.submit();
+	}
+<script type="text/javascript">
+
+
+
+
+function Pager(tableName, itemsPerPage) {
+
+this.tableName = tableName;
+
+this.itemsPerPage = itemsPerPage;
+
+this.currentPage = 1;
+
+this.pages = 0;
+
+this.inited = false;
+
+this.showRecords = function(from, to) {
+
+var rows = document.getElementById(tableName).rows;
+
+// i starts from 1 to skip table header row
+
+for (var i = 1; i < rows.length; i++) {
+
+if (i < from || i > to)
+
+rows[i].style.display = 'none';
+
+else
+
+rows[i].style.display = '';
+
+}
+
+}
+
+this.showPage = function(pageNumber) {
+
+if (! this.inited) {
+
+alert("not inited");
+
+return;
+
+}
+
+var oldPageAnchor = document.getElementById('pg'+this.currentPage);
+
+oldPageAnchor.className = 'pg-normal';
+
+this.currentPage = pageNumber;
+
+var newPageAnchor = document.getElementById('pg'+this.currentPage);
+
+newPageAnchor.className = 'pg-selected';
+
+var from = (pageNumber - 1) * itemsPerPage + 1;
+
+var to = from + itemsPerPage - 1;
+
+this.showRecords(from, to);
+
+}
+
+this.prev = function() {
+
+if (this.currentPage > 1)
+
+this.showPage(this.currentPage - 1);
+
+}
+
+this.next = function() {
+
+if (this.currentPage < this.pages) {
+
+this.showPage(this.currentPage + 1);
+
+}
+
+}
+
+this.init = function() {
+
+var rows = document.getElementById(tableName).rows;
+
+var records = (rows.length - 1);
+
+this.pages = Math.ceil(records / itemsPerPage);
+
+this.inited = true;
+
+}
+
+this.showPageNav = function(pagerName, positionId) {
+
+if (! this.inited) {
+
+alert("not inited");
+
+return;
+
+}
+
+var element = document.getElementById(positionId);
+
+var pagerHtml = '<span onclick="' + pagerName + '.prev();" class="pg-normal">< </span> ';
+
+for (var page = 1; page <= this.pages; page++)
+
+pagerHtml += '<style id="pg' + page + '" class="pg-normal;"></style> ';
+
+pagerHtml += '<span onclick="'+pagerName+'.next();" class="pg-normal"> ></span>';
+
+element.innerHTML = pagerHtml;
+
+}
+
+}
+ 
+function popup(mylink, windowname) 
+{
+	if (! window.focus)return true;
+	var href; if (typeof(mylink) == 'string') href=mylink; 
+	else href=mylink.href; 
+	window.open(href, windowname, 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=800,height=500'); 
+	return false; 
+	} 
+	</SCRIPT>
+
+ 
+
