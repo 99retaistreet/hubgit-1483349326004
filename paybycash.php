6af7dee8c7780f47c81d122d78581038543a02f9
2012rcj <2012rcj@gmail.com>
2017-01-11 22:30:43 +0530

    Update paybycash.php

--- paybycash.php
+++ paybycash.php
@@ -1,87 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>99 retail Solution Pvt Ltd</title>
-	
-    <!-- css -->
-    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-	<link rel="stylesheet" type="text/css" href="plugins/cubeportfolio/css/cubeportfolio.min.css">
-	<link href="css/nivo-lightbox.css" rel="stylesheet" />
-	<link href="css/nivo-lightbox-theme/default/default.css" rel="stylesheet" type="text/css" />
-	<link href="css/owl.carousel.css" rel="stylesheet" media="screen" />
-    <link href="css/owl.theme.css" rel="stylesheet" media="screen" />
-      <link href="simpletreemenu.js" rel="stylesheet"  />
-	<link href="css/animate.css" rel="stylesheet" />
-    <link href="css/style.css" rel="stylesheet">
-
-	<!-- boxed bg -->
-	<link id="bodybg" href="bodybg/bg1.css" rel="stylesheet" type="text/css" />
-	<!-- template skin -->
-	<link id="t-colors" href="color/default.css" rel="stylesheet">
-
-
-</head>
-
-<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
-
-<div id="wrapper">
-	
-    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
-		<div class="top-area">
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-6 col-md-6">
-					<p class="bold text-left">Monday - Saturday, 8am to 10pm </p>
-					</div>
-					<div class="col-sm-6 col-md-6">
-					<p class="bold text-right">Call us now ---</p>
-					</div>
-				</div>
-			</div>
-		</div>
-        <div class="container navigation">
-		
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
-                    <i class="fa fa-bars"></i>
-                </button>
-                <a class="navbar-brand" href="index.html">
-                    <img src="img/logo.png" alt="" width="150" height="40" />
-                </a>
-            </div>
-
-           
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
-	
-
-	<!-- Section: intro -->
-    <section id="intro" class="intro">
-	
-						<div class="intro-content">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-6">
-					<div class="wow fadeInDown" data-wow-offset="0" data-wow-delay="0.1s">
-					<h2 class="h-ultra">Hemas medical group</h2>
-					</div>
-					
-	
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-3 col-md-3">
-                                 <div class="well well-trans">
-<div class="wow fadeInRight" data-wow-delay="0.1s">
-<ul class="lead-list">
 					
 						
 							
@@ -251,10 +168,4 @@ if (!db2_execute($result)) {
 
 
 
-                                               </ul>	</ul>
-						
-					</div></div></div>
-			
-					
-
-						</div>
+                                               
