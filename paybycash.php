2dd752a1a3677962d289e55ddc4c9c09e3b1ab10
2012rcj <2012rcj@gmail.com>
2017-01-11 20:46:14 +0530

    Update paybycash.php

--- paybycash.php
+++ paybycash.php
@@ -66,36 +66,7 @@
 
 	<!-- Section: intro -->
     <section id="intro" class="intro">
-		<div class="intro-content">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-6">
-					<div class="wow fadeInDown" data-wow-offset="0" data-wow-delay="0.1s">
-					<h2 class="h-ultra">Hemas medical group</h2>
-					</div>
-					
 	
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-3 col-md-3">
-                                 <div class="well well-trans">
-<div class="wow fadeInRight" data-wow-delay="0.1s">
-<ul class="lead-list">
-					<div class="wow fadeInUp" data-wow-delay="0.2s">
-						<div class="box text-center">
-							
-							<i class="fa fa-check fa-3x circled bg-skin"></i>
-							<h4 class="h-bold"> your Overall summary </h4>
-							<p>
-							 							</p>
-
-                                               </ul>	</ul>
-						</div>
-					</div></div></div></div>
-			
-					
-
-						</div>
 						
 
 
@@ -108,7 +79,7 @@
 							<div class="panel-heading">
 									<h3 class="panel-title"><span class="fa fa-pencil-square-o"></span>your Overall Summary </h3>
 									</div>
-									<div class="panel-body">
+									<div class="panel-body" width="100%">
 
 <?php
 // include databse configuration file
