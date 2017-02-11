8af8197070be02f30fcdc2c1fc2939e5e982e49f
99retaistreet <ravi.saini@99rstreet.com>
2017-02-11 15:09:52 +0530

    Update Config.php

--- Config.php
+++ Config.php
@@ -3,5 +3,5 @@ $db_username = 'RS';
 $db_password = 'orbisindia'; 
 $db_name = 'rs'; 
 $db_host = 'retail'; 
-$db = new mysqli($db_host, $db_username, $db_password,$db_name);
+$db = new mysqli($db_host, $db_username, $db_password,$db_name);  
  ?>
